12f35431e2937b8d3350a59a151798d9d240e12b
Nancy <nbd52@yahoo.com>
2019-06-03 17:56:24 -0400

    sfg-pet-clinic: migrated from JUnit 4 to Junit 5

--- sfg-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/web/SfgPetClinicApplicationTests.java
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/web/SfgPetClinicApplicationTests.java
@@ -1,11 +1,17 @@
 package guru.springframework.sfgpetclinic.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// JUnit4 -> JUnit5 changes.  also, look at pom.xml changes.
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SfgPetClinicApplicationTests {
 
