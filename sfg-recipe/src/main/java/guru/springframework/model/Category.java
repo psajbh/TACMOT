752567382bec85b6b2f3d7900e421f0abcb28e5b
Nancy <nbd52@yahoo.com>
2019-07-30 19:33:28 -0400

    sfg-recipe jpa entity enhancements for mysql usage

--- sfg-recipe/src/main/java/guru/springframework/model/Category.java
+++ sfg-recipe/src/main/java/guru/springframework/model/Category.java
@@ -2,11 +2,14 @@ package guru.springframework.model;
 
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -17,12 +20,17 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode(exclude= {"recipes"})
 @Entity
+/*
+ * @Table(uniqueConstraints = {@UniqueConstraint(columnNames = "description",
+ * name = "uniqueNameConstraint")})
+ */
 public class Category {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(unique=true, nullable=false, length = 255)
 	private String description;
 	
 	@ManyToMany(mappedBy="categories")
--- sfg-recipe/src/main/java/guru/springframework/model/UnitOfMeasure.java
+++ sfg-recipe/src/main/java/guru/springframework/model/UnitOfMeasure.java
@@ -1,19 +1,30 @@
 package guru.springframework.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
 @Data
 @Entity
-public class UnitOfMeasure {
+/*
+ * @Table(uniqueConstraints = {@UniqueConstraint(columnNames = "uom", name =
+ * "uniqueNameConstraint")})
+ */public class UnitOfMeasure {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	
+	@NotNull
+	@Column(unique=true, nullable=false, length=255)
 	private String uom; //description
 
 }
