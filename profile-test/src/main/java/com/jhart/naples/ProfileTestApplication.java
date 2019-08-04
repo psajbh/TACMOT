b5f1ddc16b37dc0b335ce8566e28b1b41c218503
John Hart <jhart.naples@gmail.com>
2019-08-04 19:04:22 -0400

    profile-test initial code

---
+++ profile-test/src/main/java/com/jhart/naples/ProfileTestApplication.java
@@ -0,0 +1,13 @@
+package com.jhart.naples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileTestApplication.class, args);
+	}
+
+}
