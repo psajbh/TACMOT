274f76a5e215a6bc64363be2cd611568259d14b6
John Hart <jhart.naples@gmail.com>
2019-08-25 20:24:57 -0400

    hartfin template initiated

---
+++ hartfin/hartfin-web/src/main/java/com/jhart/naples/HartfinApplication.java
@@ -0,0 +1,13 @@
+package com.jhart.naples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HartfinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HartfinApplication.class, args);
+	}
+
+}
---
+++ hartfin/hartfin-web/src/test/java/com/jhart/naples/HartfinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jhart.naples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HartfinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
