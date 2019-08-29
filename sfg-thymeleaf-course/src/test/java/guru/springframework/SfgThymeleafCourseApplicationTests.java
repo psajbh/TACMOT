7288c33de3960cf610bc47856ba373e8a15d843a
John Hart <jhart.naples@gmail.com>
2019-08-28 21:08:11 -0400

    sfg-thymeleaf initial code

---
+++ sfg-thymeleaf-course/src/test/java/guru/springframework/SfgThymeleafCourseApplicationTests.java
@@ -0,0 +1,21 @@
+package guru.springframework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@SpringApplicationConfiguration(classes = SfgThymeleafCourseApplication.class)
+//@WebAppConfiguration
+public class SfgThymeleafCourseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
