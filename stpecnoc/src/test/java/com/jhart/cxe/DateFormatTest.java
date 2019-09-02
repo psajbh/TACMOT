268481ce67cf7402a20f64789ab870f0a1a98eae
John Hart <jhart.naples@gmail.com>
2019-09-02 19:30:44 -0400

    stpecnoc: added more test examples

---
+++ stpecnoc/src/test/java/com/jhart/cxe/DateFormatTest.java
@@ -0,0 +1,52 @@
+package com.jhart.cxe;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import org.junit.Test;
+
+public class DateFormatTest {
+	
+	private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
+	private static final DateTimeFormatter dateFormatterNew = DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy HH:mm:ss a");
+	
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+    private static final SimpleDateFormat sdfNew = 	new SimpleDateFormat("EEEE, MMM d, yyyy HH:mm:ss a");
+	
+	@Test
+	public void testDateFormat() throws Exception{
+		
+		String date = "2019-05-23 00:00:00.0";
+
+		// string to LocalDateTime
+        LocalDateTime ldateTime = LocalDateTime.parse(date, dateFormatter);
+
+        System.out.println(dateFormatter.format(ldateTime));
+
+        // change date format
+        System.out.println(dateFormatterNew.format(ldateTime));
+	}
+	
+	@Test
+	public void testSimpleDateFormat() throws Exception{
+		
+		String dateString = "2019-05-23 00:00:00.0";
+
+        try {
+
+			// string to date
+            Date date = sdf.parse(dateString);
+
+            System.out.println(sdf.format(date));
+			
+            System.out.println(sdfNew.format(date));
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+	}
+
+}
