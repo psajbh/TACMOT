4b5520274d70e2f85e864f2f3ee65f2fbf091f67
John Hart <jhart.naples@gmail.com>
2019-09-15 20:00:20 -0400

    my-mongo integrated update capability

--- my-mongo/src/main/java/com/jhart/web/UpdateController.java
+++ my-mongo/src/main/java/com/jhart/web/UpdateController.java
@@ -1,10 +1,14 @@
 package com.jhart.web;
 
+import java.util.Date;
+
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.jhart.domain.Todo;
 import com.jhart.service.TodoService;
@@ -24,10 +28,41 @@ public class UpdateController {
 	@GetMapping("todo/update/{id}")
 	public String updateTodo(Model model, @PathVariable ObjectId id) {
 		log.debug("updateTodo: start");
-
-		
 		Todo todo = todoService.findById(id);
-		//model.addAttribute("todos", todoItems);
+		model.addAttribute("todo", todo);
+		return "updateTodo";
+	}
+	
+	@PostMapping("todo/update/save")
+	public String save(Model model, Todo formTodo) {
+		log.debug("save: formTodo: " + formTodo.toString());
+		Todo updateTodo = todoService.findById(formTodo.getId());
+		if (null == updateTodo.getCreateDate()){
+			updateTodo.setCreateDate(new Date());
+		}
+		
+		updateTodo.setTaskName(formTodo.getTaskName());
+		updateTodo.setOwner(formTodo.getOwner());
+		
+		boolean formComplete = formTodo.isCompleted();
+		boolean updateComplete = updateTodo.isCompleted();
+		
+		if (formComplete != updateComplete) {
+			if (formComplete) {
+				updateTodo.setCompleted(true);
+				updateTodo.setCompletedDate(new Date());
+			}
+			else {
+				updateTodo.setCompleted(false);
+				updateTodo.setCompletedDate(null);
+			}
+		}
+		
+		updateTodo.setCompleted(formTodo.isCompleted());
+		todoService.save(updateTodo);
+		
+		Iterable<Todo> todoItems = todoService.listAll();
+		model.addAttribute("todos", todoItems);
 		return "index";
 	}
 
