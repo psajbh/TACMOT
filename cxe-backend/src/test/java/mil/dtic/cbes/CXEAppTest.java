b3e13c477011a5a5b9e03ac129f145f779999dce
John Hart <jhart.naples@gmail.com>
2020-01-27 08:05:15 -0500

    cxe-backend CXEAppTest integrated

---
+++ cxe-backend/src/test/java/mil/dtic/cbes/CXEAppTest.java
@@ -0,0 +1,14 @@
+package mil.dtic.cbes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CXEAppTest {
+	
+	
+	@Test
+	public void contextLoads() {
+	}
+
+}
