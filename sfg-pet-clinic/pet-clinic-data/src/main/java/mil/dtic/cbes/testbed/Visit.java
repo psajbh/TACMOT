bf24339f0d610d5f294780c5329527e35f159ee4
Nancy <nbd52@yahoo.com>
2020-01-30 07:59:05 -0500

    cxe-backend refactored testbed Visit.java

--- sfg-pet-clinic/pet-clinic-data/src/main/java/mil/dtic/cbes/testbed/Visit.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/mil/dtic/cbes/testbed/Visit.java
@@ -1,4 +1,4 @@
-package guru.springframework.sfgpetclinic.model;
+package mil.dtic.cbes.testbed;
 
 import java.time.LocalDate;
 
@@ -8,34 +8,50 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import guru.springframework.sfgpetclinic.model.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Entity
-@Table(name="visits")
-public class Visit extends BaseEntity{
-	private static final long serialVersionUID = -989439934030766620L;
+public class Visit extends BaseEntity {
 	
-	@Column(name="date")
 	private LocalDate date;
-	
-	@Column(name="description")
-	private String description;
-	
-	@ManyToOne
-	@JoinColumn(name="pet_id")
-	private Pet pet;
-	
-	public LocalDate getDate() {
-		return date;
-	}
+    private String description;
+    private Pet pet;
+    
+    public Visit(Long id) {
+        super(id);
+    }
+
+    public Visit(Long id, LocalDate date) {
+        super(id);
+        this.date = date;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
 
 }
